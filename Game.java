39c8a5891e6a01a95328ac2bccf899eb9a58e992
Ppiinnkkk <73686006+Ppiinnkkk@users.noreply.github.com>
2021-05-31 19:39:25 +0700

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,1 +1,48 @@
+/*
+     * If a player clicks on a zero, all surrounding cells ("neighbours") must revealed.
+     * This method is recursive: if a neighbour is also a zero, his neighbours must also be revealed.
+     */
+    public void findZeroes(int xCo, int yCo)
+    {
+        int neighbours;
 
+        Cell cells[][] = board.getCells();
+        JButton buttons[][] = gui.getButtons();
+
+        // Columns
+        for(int x = board.makeValidCoordinateX(xCo - 1) ; x <= board.makeValidCoordinateX(xCo + 1) ; x++)
+        {
+            // Rows
+            for(int y = board.makeValidCoordinateY(yCo - 1) ; y <= board.makeValidCoordinateY(yCo + 1) ; y++)
+            {
+                // Only unrevealed cells need to be revealed.
+                if(cells[x][y].getContent().equals(""))
+                {
+                    // Get the neighbours of the current (neighbouring) cell.
+                    neighbours = cells[x][y].getSurroundingMines();
+
+                    // Reveal the neighbours of the current (neighbouring) cell
+                    cells[x][y].setContent(Integer.toString(neighbours));
+
+                    if (!cells[x][y].getMine())
+                        buttons[x][y].setIcon(null);
+
+                    // Is this (neighbouring) cell a "zero" cell itself?
+                    if(neighbours == 0)
+                    {
+                        // Yes, give it a special color and recurse!
+                        buttons[x][y].setBackground(Color.lightGray);
+                        buttons[x][y].setText("");
+                        findZeroes(x, y);
+                    }
+                    else
+                    {
+                        // No, give it a boring gray color.
+                        buttons[x][y].setBackground(Color.lightGray);
+                        buttons[x][y].setText(Integer.toString(neighbours));
+                        gui.setTextColor(buttons[x][y]);
+                    }
+                }
+            }
+        }
+    }

a31c90f928589536421f5593c9599e86df0936f0
latringuyen <74802763+latringuyen@users.noreply.github.com>
2021-06-06 15:46:12 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+package Project;
+
+import java.io.IOException;
+
+public class Main
+{
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        Game game = new Game();
+    }
+}
+
